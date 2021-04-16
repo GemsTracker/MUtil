49e1f21aa6091bf2859a9f5624025ea32e16d68e
mddejong <mjong@magnafacta.nl>
2021-04-16 19:59:41 +0200

    Added description to composer and fixed some spelling stuff

--- src/MUtil/Model/Dependency/DependencyAbstract.php
+++ src/MUtil/Model/Dependency/DependencyAbstract.php
@@ -1,38 +1,12 @@
 <?php
 
 /**
- * Copyright (c) 2014, Erasmus MC
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *    * Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *    * Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in the
- *      documentation and/or other materials provided with the distribution.
- *    * Neither the name of Erasmus MC nor the
- *      names of its contributors may be used to endorse or promote products
- *      derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
  *
  * @package    MUtil
  * @subpackage Model_Dependency
  * @author     Matijs de Jong <mjong@magnafacta.nl>
  * @copyright  Copyright (c) 2014 Erasmus MC
  * @license    New BSD License
- * @version    $Id: DependencyAbstract .php 1748 2014-02-19 18:09:41Z matijsdejong $
  */
 
 namespace MUtil\Model\Dependency;
@@ -52,7 +26,7 @@ abstract class DependencyAbstract extends \MUtil_Translate_TranslateableAbstract
     /**
      * Array of setting => setting of setting changed by this dependency
      *
-     * The settings array for those effecteds that don't have an effects array
+     * The settings array for those effected items that don't have an effects array
      *
      * @var array
      */
@@ -61,7 +35,7 @@ abstract class DependencyAbstract extends \MUtil_Translate_TranslateableAbstract
     /**
      * Array of name => name of items dependency depends on.
      *
-     * Can be overriden in sub class, when set to only field names this class will
+     * Can be overridden in sub class, when set to only field names this class will
      * change the array to the correct structure.
      *
      * @var array Of name => name
@@ -71,7 +45,7 @@ abstract class DependencyAbstract extends \MUtil_Translate_TranslateableAbstract
     /**
      * Array of name => array(setting => setting) of fields with settings changed by this dependency
      *
-     * Can be overriden in sub class, when set to only field names this class will use _defaultEffects
+     * Can be overridden in sub class, when set to only field names this class will use _defaultEffects
      * to change the array to the correct structure.
      *
      * @var array of name => array(setting => setting)
