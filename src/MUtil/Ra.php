cfbc20e9a98debdff87e79d7e762e076e08838e3
mddejong <mjong@magnafacta.nl>
2021-04-18 20:21:52 +0200

    New utility functions for array manipulation

--- src/MUtil/Ra.php
+++ src/MUtil/Ra.php
@@ -1,38 +1,12 @@
 <?php
 
 /**
- * Copyright (c) 2011, Erasmus MC
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *    * Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *    * Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in the
- *      documentation and/or other materials provided with the distribution.
- *    * Neither the name of Erasmus MC nor the
- *      names of its contributors may be used to endorse or promote products
- *      derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
  *
  * @package    MUtil
  * @subpackage Ra
  * @author     Matijs de Jong <mjong@magnafacta.nl>
  * @copyright  Copyright (c) 2011 Erasmus MC
  * @license    New BSD License
- * @version    $Id$
  */
 
 /**
@@ -516,9 +490,27 @@ class MUtil_Ra
     }
 
     /**
+     * @param array $targetArray
+     * @param scalar $findKey
+     * @return false|int The integer position of the key or false if not found
+     */
+    public static function getKeyPos(array $targetArray, $findKey)
+    {
+        $pos = 0;
+        foreach ($targetArray as $key => $val) {
+            if ($key === $findKey) {
+                return $pos;
+            }
+            $pos++;
+        }
+        
+        return false;
+    }
+    
+    /**
      * Get or create the current to ArrayConverter
      *
-     * @param mxied $converter \MUtil_Util_ClassList or something that can be used as input to create one
+     * @param mixed $converter \MUtil_Util_ClassList or something that can be used as input to create one
      */
     public static function getToArrayConverter()
     {
@@ -529,6 +521,62 @@ class MUtil_Ra
         return self::$_toArrayConverter;
     }
 
+    /**
+     * Insert a value after a certain key occurs (or at the end if it does not occur)
+     * 
+     * @param array $targetArray The array to insert in
+     * @param scalar $afterKey The key to insert after
+     * @param mixed $value The value to insert
+     * @param scalar $keyValue The new key for the value
+     * @return array
+     */    
+    public static function insertAfter(array $targetArray, $afterKey, $value, $keyValue = null)
+    {
+        if (null === $keyValue) {
+            $newArray[count($targetArray)] = $value;
+        } else {
+            $newArray[$keyValue] = $value;
+        }
+        $pos = self::getKeyPos($targetArray, $afterKey);
+
+        if (false === $pos) {
+            return $targetArray + $newArray;
+        }
+
+        array_splice($targetArray, $pos, 0, $newArray);
+        
+        return $targetArray;
+    }
+
+    /**
+     * Insert a value after a certain key occurs (or at the start if it does not not occur)
+     *
+     * @param array $targetArray The array to insert in
+     * @param scalar $beforeKey The key to insert before
+     * @param mixed $value The value to insert
+     * @param scalar $keyValue The new key for the value
+     * @return array
+     */
+    public static function insertBefore(array $targetArray, $beforeKey, $value, $keyValue = null)
+    {
+        if (null === $keyValue) {
+            $newArray[count($targetArray)] = $value;
+        } else {
+            $newArray[$keyValue] = $value;
+        }
+        $pos = self::getKeyPos($targetArray, $beforeKey);
+
+        if (false === $pos) {
+            return $newArray + $targetArray;
+        }
+        
+        $pos--;
+        
+        array_splice($targetArray, $pos, 0, $newArray);
+
+        return $targetArray;
+    }
+    
     /**
      * Returns true if the $object either is an array or can be converted to an array.
      *
